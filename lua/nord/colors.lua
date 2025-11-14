f400059d15e65c57d7e23c459dd9a9c653df28a6
divalin <yosefkesef10@gmail.com>
2025-11-14 10:50:29 +0000

    Change nord1_gui color to glacier

--- lua/nord/colors.lua
+++ lua/nord/colors.lua
@@ -23,7 +23,7 @@ local dark_colors = {
 
 local light_colors = {
 	nord0_gui = named_colors.glacier, -- nord0 in palette
-	nord1_gui = named_colors.darker_white, -- nord1 in palette
+	nord1_gui = named_colors.glacier, -- nord1 in palette
 	nord2_gui = named_colors.darkest_white, -- nord2 in palette
 	nord3_gui = named_colors.light_gray, -- nord3 in palette
 	nord3_gui_bright = named_colors.light_gray_bright, -- out of palette
